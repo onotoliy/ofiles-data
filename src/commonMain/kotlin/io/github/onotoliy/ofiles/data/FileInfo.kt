65c5940d901492a9317fd106c9b4d0309f3e717c
Anatoliy Pokhresniy <a.k.pohresniy@gmail.com>
2022-08-31 11:13:51 +0800

    init project

---
+++ src/commonMain/kotlin/io/github/onotoliy/ofiles/data/FileInfo.kt
@@ -0,0 +1,39 @@
+package io.github.onotoliy.ofiles.data
+
+import io.github.onotoliy.core.data.HasAuthor
+import io.github.onotoliy.core.data.HasCreationDate
+import io.github.onotoliy.core.data.HasName
+import io.github.onotoliy.core.data.HasUID
+import io.github.onotoliy.core.data.Option
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+
+/**
+ * Ошибка произощедшая на устройстве.
+ *
+ * @param uid Уникальный идентификатор.
+ * @param account Аккаунт.
+ * @param author Автор.
+ * @param contentType ContentType.
+ * @param description Описание.
+ * @param extension Расширение.
+ * @param name Название.
+ * @param originalName Оригинальное название.
+ * @param size Размер.
+ * @param creationDate Дата создания.
+ * @param parameters Дополнительные параметры.
+ * @author Anatoliy Pokhresniy
+ */
+data class FileInfo(
+    override val uid: String = "",
+    override val author: Option = Option(),
+    override val creationDate: Instant = Clock.System.now(),
+    override val name: String = "",
+    val originalName: String = "",
+    val description: String = "",
+    val account: String = "",
+    val extension: String = "",
+    val contentType: String = "",
+    val size: Long = 0,
+    val parameters: List<Option> = listOf()
+): HasUID, HasName, HasAuthor, HasCreationDate
